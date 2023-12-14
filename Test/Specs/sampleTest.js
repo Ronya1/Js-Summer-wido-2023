9198f8d4d5e4ef5c1391d19d4be413d99ac30b7b
RON - COD <ron-coding@Ronalds-MBP.lan>
2023-12-14 10:50:50 -0500

    Debugging Setup

---
+++ Test/Specs/sampleTest.js
@@ -0,0 +1,287 @@
+// const sampleTest = require('./sampleTest');
+// const { expect , assert } = require("chai");
+// const describe  = require('mocha'); -> FUCKING REMOVE THIS!!!!!! WHY THE FUCK IS IT HERE? 
+
+// // describe section : Feature
+// describe('Login', () => {
+//     // it-block : defines a testcase
+//     // it('Verify user can login with valid credentials', () => {
+        
+//     //     const res = sampleTest.addNumbers(2,5);
+
+//     //     expect(res, "expect - addFunction does not work as expected").to.equal(7);
+//     //     assert.equal(res, 7, "assert - addFunction does not work as expected")
+
+//     //     expect(1===1, "fail the testcase").to.be.true;
+
+//     //     expect(1===2, "fail the testcase").to.be.false;
+
+//     // });
+//     //HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+//     before(async () => {
+//         // This code will run once before any test in this describe block
+//         await browser.url('https://www.facebook.com');
+//         // You can add any other setup logic here
+//         // ...
+//     });
+
+//     it('Verify the page title', async () => {
+//         /**
+//          * To launch a webpage
+//          * function: url
+//          */
+//         //await browser.url('https://www.facebook.com');
+//         // before(async () => {
+//         //     // This code will run once before any test in this describe block
+//         //     await browser.url('https://www.facebook.com');
+//         //     // You can add any other setup logic here
+//         //     // ...
+//         // });
+
+//         /**
+//          * To pause the execution
+//          * function: pause
+//          */
+//         // await browser.pause(60000);
+
+//         /**
+//          * To get the page title
+//          * function: getTitle()
+//          */
+//         const pageTitle = await browser.getTitle();
+
+//         console.log('\nPage title');
+//         console.log(pageTitle);
+//         console.log('Page title\n');
+
+//         expectedPageTitle = 'Facebook – log in or sign up';
+//         expect(pageTitle, 'Facebook title is not as expected').to.equal(expectedPageTitle);       
+
+//     });
+
+//     it('Verify the correct url launches when we open a webpage', async () => {
+//         /**
+//          * Steps:
+//          *  1. Open webpage
+//          *  2. launch the webpage
+//          *  3. Verify opened url is equal to url we launched
+//          */
+
+//         // 1 and 2
+//         const launchUrl = 'https://www.facebook.com/';
+//         await browser.url(launchUrl);
+
+//         /**
+//          * To get the current url from webpage
+//          * function: getUrl()
+//          */
+//         const launchedUrl = await browser.getUrl();
+
+//         expect(launchedUrl, 'Correct url is NOT launched when we open a webpage').to.equal(launchUrl);
+
+//     });
+
+//     it('Misc functions from browser', async () => {
+//         const launchUrl = 'https://www.facebook.com/';
+//         await browser.url(launchUrl);
+
+//         /**
+//          * To get the windowHandle stored in browser-object
+//          * function: getWindowHandle()
+//          */
+//         const windowHandle = await browser.getWindowHandle();
+//         console.log('\nwindowHandle');
+//         console.log(windowHandle);
+//         console.log('windowHandle\n');
+
+//         await browser.pause(7000);
+
+//         /**
+//          * To go back in browser
+//          * function: back()
+//          */
+//         await browser.back();
+
+//         await browser.pause(7000);
+
+//         /**
+//          * To go forward in browser
+//          * function: forward()
+//          */
+//         await browser.forward();
+
+//         await browser.pause(7000);
+
+//         /**
+//          * To refresh a webpage
+//          * function: refresh()
+//          */
+//         await browser.refresh();
+
+//         /**
+//          * Other ways to refresh a webpage
+//          * 1. re-launch the url
+//          * 2. back() then forward()
+//          */
+//         await browser.pause(7000);
+
+//     });
+
+// });
+
+// // Create a “describe” block
+// // 12. Create an “it” block
+// // 13. Automate below testcase:
+// //     Verify facebook title contains LoG In (ignore cases)
+// //     Steps:
+// //     1. Launch facebook.com
+// //     2. Get the page title
+// //     3. Verify pageTitle contains “LoG In” (ignoring cases)
+
+// /**
+//  * launch new browser window
+//  * launch facebook.com 
+//  * get the page title 
+//  * compare page title with expected result 
+//  */
+
+
+// describe('Launch Website andverify title', () => {
+
+//     it('Verify the page title', async () => { // if I didnt do it.only it did not execute the test case. 
+//         const webpage = ('https://www.apple.com/')
+//         await browser.url(webpage)
+//         await browser.pause(7000);
+
+//         const pageTitle = await browser.getTitle();
+
+//         console.log('\nPage title');
+//         console.log(pageTitle);
+//         console.log('Page title\n');
+
+//         expectedPageTitle = 'Bpple';
+//         expect(pageTitle, 'Apple title is not as expected').to.equal(expectedPageTitle);  
+
+
+//     })
+
+
+
+
+// })
+
+
+//NEW VERSION: 
+// const sampleTest = require('./sampleTest');
+// const { expect, assert } = require('chai');
+// const describe = require('mocha');
+
+// describe('Login', () => {
+    
+//     before(async () => {
+//         console.log("ABOUT TO OPEN1")
+//         await browser.url('https://www.facebook.com');
+//     });
+//     it('Verify the page title', async () => {
+//         console.log("TESTING")
+//         const pageTitle = await browser.getTitle();
+
+//         console.log('\nPage title');
+//         console.log(pageTitle);
+//         console.log('Page title\n');
+
+//         expectedPageTitle = 'Facebook – log in or sign up';
+//         expect(pageTitle, 'Facebook title is not as expected').to.equal(expectedPageTitle);
+//     });
+
+    // it('Verify the correct url launches when we open a webpage', async () => {
+    //     const launchUrl = 'https://www.facebook.com/';
+    //    // await browser.url(launchUrl);
+
+    //     const launchedUrl = await browser.getUrl();
+
+    //     expect(launchedUrl, 'Correct url is NOT launched when we open a webpage').to.equal(launchUrl);
+    // });
+
+    // it('Misc functions from browser', async () => {
+    //     const launchUrl = 'https://www.facebook.com/';
+    //    // await browser.url(launchUrl);
+
+    //     const windowHandle = await browser.getWindowHandle();
+    //     console.log('\nwindowHandle');
+    //     console.log(windowHandle);
+    //     console.log('windowHandle\n');
+
+    //     await browser.pause(7000);
+
+    //     await browser.back();
+
+    //     await browser.pause(7000);
+
+    //     await browser.forward();
+
+    //     await browser.pause(7000);
+
+    //     await browser.refresh();
+
+    //     await browser.pause(7000);
+    // });
+// });
+
+// Describe block for launching and verifying title
+// describe('Launch Website and verify title', () => {
+//     it('Verify the page title', async () => {
+//         const webpage = 'https://www.apple.com/';
+//         //await browser.url(webpage);
+//         await browser.pause(7000);
+
+//         const pageTitle = await browser.getTitle();
+
+//         console.log('\nPage title');
+//         console.log(pageTitle);
+//         console.log('Page title\n');
+
+//         expectedPageTitle = 'Apple';
+//         expect(pageTitle, 'Apple title is not as expected').to.equal(expectedPageTitle);
+//     });
+// });
+
+
+//V3
+const { expect, assert } = require('chai');
+const sampleTest = require('./sampleTest');
+
+
+describe('Login', () => {
+    before(async () => {
+        console.log("ABOUT TO OPEN1");
+        await browser.url('https://www.facebook.com');
+    });
+
+    it('Verify the page title', async () => {
+        console.log("TESTING");
+        const pageTitle = await browser.getTitle();
+
+        console.log('\nPage title');
+        console.log(pageTitle);
+        console.log('Page title\n');
+
+        expectedPageTitle = 'Facebook – log in or sign up';
+        expect(pageTitle, 'Facebook title is not as expected').to.equal(expectedPageTitle);
+    });
+
+    // Additional tests can be added here using "it" blocks
+
+    // Add other test hooks as needed
+
+    // Example:
+    // afterEach(async () => {
+    //     // Code to run after each test
+    // });
+
+    // Example:
+    // after(async () => {
+    //     // Code to run after all tests
+    // });
+});
+
--- wdio.conf.js
+++ wdio.conf.js
@@ -3,14 +3,13 @@ exports.config = {
     // ====================
     // Runner Configuration
     // ====================
-    // WebdriverIO supports running e2e tests as well as unit and component tests.
-    runner: 'local',
+    //
     //
     // ==================
     // Specify Test Files
     // ==================
     // Define which test specs should run. The pattern is relative to the directory
-    // of the configuration file being run.
+    // from which `wdio` was called.
     //
     // The specs are defined as an array of spec files (optionally using wildcards
     // that will be expanded). The test for each spec file will be run in a separate
@@ -22,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        // ToDo: define location for spec files here
+        './test/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -51,9 +50,19 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+    
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 5,
+        //
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+        // If outputDir is provided WebdriverIO can capture driver session logs
+        // it is possible to configure which logTypes to include/exclude.
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['bugreport', 'server'],
     }],
-
     //
     // ===================
     // Test Configurations
@@ -85,7 +94,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://www.facebook.com/',
+    baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -101,8 +110,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['vscode'],
-
+    services: ['chromedriver'],
+    
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
@@ -110,7 +119,6 @@ exports.config = {
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
     framework: 'mocha',
-    
     //
     // The number of times to retry the entire specfile when it fails as a whole
     // specFileRetries: 1,
@@ -118,7 +126,7 @@ exports.config = {
     // Delay in seconds between the spec file retry attempts
     // specFileRetriesDelay: 0,
     //
-    // Whether or not retried spec files should be retried immediately or deferred to the end of the queue
+    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
     // specFileRetriesDeferred: false,
     //
     // Test reporter for stdout.
@@ -126,13 +134,22 @@ exports.config = {
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: ['spec'],
 
+
+    
+    //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
-
+    before: async () => {
+        // This code will run once before any test in this describe block
+        console.log("ABOUT TO OPEN");
+        await browser.url('https://www.facebook.com');
+        // You can add any other setup logic here
+        // ...
+    },
     //
     // =====
     // Hooks
@@ -143,38 +160,38 @@ exports.config = {
     // resolved to continue.
     /**
      * Gets executed once before all workers get launched.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      */
     // onPrepare: function (config, capabilities) {
     // },
     /**
-     * Gets executed before a worker process is spawned and can be used to initialize specific service
+     * Gets executed before a worker process is spawned and can be used to initialise specific service
      * for that worker as well as modify runtime environments in an async fashion.
-     * @param  {string} cid      capability id (e.g 0-0)
-     * @param  {object} caps     object containing capabilities for session that will be spawn in the worker
-     * @param  {object} specs    specs to be run in the worker process
-     * @param  {object} args     object that will be merged with the main configuration once worker is initialized
-     * @param  {object} execArgv list of string arguments passed to the worker process
+     * @param  {String} cid      capability id (e.g 0-0)
+     * @param  {[type]} caps     object containing capabilities for session that will be spawn in the worker
+     * @param  {[type]} specs    specs to be run in the worker process
+     * @param  {[type]} args     object that will be merged with the main configuration once worker is initialized
+     * @param  {[type]} execArgv list of string arguments passed to the worker process
      */
     // onWorkerStart: function (cid, caps, specs, args, execArgv) {
     // },
     /**
      * Gets executed just after a worker process has exited.
-     * @param  {string} cid      capability id (e.g 0-0)
-     * @param  {number} exitCode 0 - success, 1 - fail
-     * @param  {object} specs    specs to be run in the worker process
-     * @param  {number} retries  number of retries used
+     * @param  {String} cid      capability id (e.g 0-0)
+     * @param  {Number} exitCode 0 - success, 1 - fail
+     * @param  {[type]} specs    specs to be run in the worker process
+     * @param  {Number} retries  number of retries used
      */
     // onWorkerEnd: function (cid, exitCode, specs, retries) {
     // },
     /**
      * Gets executed just before initialising the webdriver session and test framework. It allows you
      * to manipulate configurations depending on the capability or spec.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
-     * @param {string} cid worker id (e.g. 0-0)
+     * @param {String} cid worker id (e.g. 0-0)
      */
     // beforeSession: function (config, capabilities, specs, cid) {
     // },
@@ -183,20 +200,20 @@ exports.config = {
      * variables like `browser`. It is the perfect place to define custom commands.
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs        List of spec file paths that are to be run
-     * @param {object}         browser      instance of created browser/device session
+     * @param {Object}         browser      instance of created browser/device session
      */
     // before: function (capabilities, specs) {
     // },
     /**
      * Runs before a WebdriverIO command gets executed.
-     * @param {string} commandName hook command name
+     * @param {String} commandName hook command name
      * @param {Array} args arguments that command would receive
      */
     // beforeCommand: function (commandName, args) {
     // },
     /**
      * Hook that gets executed before the suite starts
-     * @param {object} suite suite details
+     * @param {Object} suite suite details
      */
     // beforeSuite: function (suite) {
     // },
@@ -209,23 +226,23 @@ exports.config = {
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
      */
-    // beforeHook: function (test, context, hookName) {
+    // beforeHook: function (test, context) {
     // },
     /**
      * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
      * afterEach in Mocha)
      */
-    // afterHook: function (test, context, { error, result, duration, passed, retries }, hookName) {
+    // afterHook: function (test, context, { error, result, duration, passed, retries }) {
     // },
     /**
      * Function to be executed after a test (in Mocha/Jasmine only)
-     * @param {object}  test             test object
-     * @param {object}  context          scope object the test was executed with
+     * @param {Object}  test             test object
+     * @param {Object}  context          scope object the test was executed with
      * @param {Error}   result.error     error object in case the test fails, otherwise `undefined`
-     * @param {*}       result.result    return object of test function
-     * @param {number}  result.duration  duration of test
-     * @param {boolean} result.passed    true if test has passed, otherwise false
-     * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
+     * @param {Any}     result.result    return object of test function
+     * @param {Number}  result.duration  duration of test
+     * @param {Boolean} result.passed    true if test has passed, otherwise false
+     * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
     // afterTest: function(test, context, { error, result, duration, passed, retries }) {
     // },
@@ -233,23 +250,23 @@ exports.config = {
 
     /**
      * Hook that gets executed after the suite has ended
-     * @param {object} suite suite details
+     * @param {Object} suite suite details
      */
     // afterSuite: function (suite) {
     // },
     /**
      * Runs after a WebdriverIO command gets executed
-     * @param {string} commandName hook command name
+     * @param {String} commandName hook command name
      * @param {Array} args arguments that command would receive
-     * @param {number} result 0 - command success, 1 - command error
-     * @param {object} error error object if any
+     * @param {Number} result 0 - command success, 1 - command error
+     * @param {Object} error error object if any
      */
     // afterCommand: function (commandName, args, result, error) {
     // },
     /**
      * Gets executed after all tests are done. You still have access to all global variables from
      * the test.
-     * @param {number} result 0 - test pass, 1 - test fail
+     * @param {Number} result 0 - test pass, 1 - test fail
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
@@ -257,7 +274,7 @@ exports.config = {
     // },
     /**
      * Gets executed right after terminating the webdriver session.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
@@ -266,8 +283,8 @@ exports.config = {
     /**
      * Gets executed after all workers got shut down and the process is about to exit. An error
      * thrown in the onComplete hook will result in the test run failing.
-     * @param {object} exitCode 0 - success, 1 - fail
-     * @param {object} config wdio configuration object
+     * @param {Object} exitCode 0 - success, 1 - fail
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
@@ -275,21 +292,9 @@ exports.config = {
     // },
     /**
     * Gets executed when a refresh happens.
-    * @param {string} oldSessionId session ID of the old session
-    * @param {string} newSessionId session ID of the new session
+    * @param {String} oldSessionId session ID of the old session
+    * @param {String} newSessionId session ID of the new session
     */
     // onReload: function(oldSessionId, newSessionId) {
-    // }
-    /**
-    * Hook that gets executed before a WebdriverIO assertion happens.
-    * @param {object} params information about the assertion to be executed
-    */
-    // beforeAssertion: function(params) {
-    // }
-    /**
-    * Hook that gets executed after a WebdriverIO assertion happened.
-    * @param {object} params information about the assertion that was executed, including its results
-    */
-    // afterAssertion: function(params) {
     // }
 }
